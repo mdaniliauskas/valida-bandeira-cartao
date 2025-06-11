714fe76ccfa0b4e865d962a0e517e62edd3273bb
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2025-06-10 23:52:50 -0300

    feat: adiciona classe Main básica para demonstração
    - Cria estrutura inicial da classe Main com método main()
    - Adiciona mensagem de apresentação do validador
    - Prepara base para implementação futura dos testes de bandeiras

---
+++ src/main/java/com/valida/cartao/Main.java
@@ -0,0 +1,8 @@
+package com.valida.cartao;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Validador de Bandeiras de Cartão de Crédito");
+        // TODO: Implementar testes
+    }
+}
